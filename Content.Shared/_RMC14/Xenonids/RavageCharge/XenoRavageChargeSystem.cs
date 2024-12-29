781308533089448a76d83927e9d764c7c40a327d
MalorMorfin <mackcimmm@mail.ru>
2024-12-30 03:24:04 +1000

    start

---
+++ Content.Shared/_RMC14/Xenonids/RavageCharge/XenoRavageChargeSystem.cs
@@ -0,0 +1,139 @@
+﻿using Content.Shared._RMC14.Marines;
+using Content.Shared._RMC14.Pulling;
+using Content.Shared._RMC14.Xenonids.Animation;
+using Content.Shared._RMC14.Xenonids.Plasma;
+using Content.Shared.Coordinates;
+using Content.Shared.Damage;
+using Content.Shared.DoAfter;
+using Content.Shared.Effects;
+using Content.Shared.FixedPoint;
+using Content.Shared.Stunnable;
+using Content.Shared.Throwing;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Network;
+using Robust.Shared.Physics.Components;
+using Robust.Shared.Player;
+using Robust.Shared.Timing;
+
+namespace Content.Shared._RMC14.Xenonids.RavageCharge;
+
+public sealed class XenoRavageChargeSystem : EntitySystem
+{
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly SharedColorFlashEffectSystem _colorFlash = default!;
+    [Dependency] private readonly DamageableSystem _damageable = default!;
+    [Dependency] private readonly INetManager _net = default!;
+    [Dependency] private readonly SharedStunSystem _stun = default!;
+    [Dependency] private readonly ThrowingSystem _throwing = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly ThrownItemSystem _thrownItem = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly XenoAnimationsSystem _xenoAnimations = default!;
+    [Dependency] private readonly XenoSystem _xeno = default!;
+    [Dependency] private readonly XenoPlasmaSystem _xenoPlasma = default!;
+    [Dependency] private readonly RMCPullingSystem _rmcPulling = default!;
+
+    private EntityQuery<PhysicsComponent> _physicsQuery;
+    private EntityQuery<ThrownItemComponent> _thrownItemQuery;
+
+    public override void Initialize()
+    {
+        _physicsQuery = GetEntityQuery<PhysicsComponent>();
+        _thrownItemQuery = GetEntityQuery<ThrownItemComponent>();
+
+        SubscribeLocalEvent<XenoRavageChargeComponent, XenoRavageChargeActionEvent>(OnXenoRavageChargeAction);
+        SubscribeLocalEvent<XenoRavageChargeComponent, ThrowDoHitEvent>(OnXenoRavageChargeHit);
+    }
+
+    private void OnXenoRavageChargeAction(Entity<XenoRavageChargeComponent> xeno, ref XenoRavageChargeActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        var attempt = new XenoRavageChargeAttemptEvent();
+        RaiseLocalEvent(xeno, ref attempt);
+
+        if (attempt.Cancelled)
+            return;
+
+        if (!_xenoPlasma.TryRemovePlasmaPopup(xeno.Owner, xeno.Comp.PlasmaCost))
+            return;
+
+        args.Handled = true;
+
+        var ev = new XenoRavageChargeDoAfterEvent(GetNetCoordinates(args.Target));
+        var doAfter = new DoAfterArgs(EntityManager, xeno, xeno.Comp.ChargeDelay, ev, xeno)
+        {
+            BreakOnMove = true,
+            Hidden = true,
+        };
+
+        _stun.TrySlowdown(xeno, TimeSpan.FromSeconds(1.75f), false, 0f, 0f);
+        _doAfter.TryStartDoAfter(doAfter);
+    }
+
+    private void OnXenoRavageChargeDoAfterEvent(Entity<XenoRavageChargeComponent> xeno, ref XenoRavageChargeDoAfterEvent args)
+    {
+        if (args.Cancelled)
+            return;
+
+        _rmcPulling.TryStopAllPullsFromAndOn(xeno);
+
+        var coordinates = GetCoordinates(args.Coordinates);
+        var origin = _transform.GetMapCoordinates(xeno);
+        var diff = _transform.ToMapCoordinates(coordinates).Position - origin.Position;
+        diff = diff.Normalized() * xeno.Comp.Range;
+
+        xeno.Comp.Charge = diff;
+        Dirty(xeno);
+
+        _throwing.TryThrow(xeno, diff, xeno.Comp.Strength, animated: false);
+    }
+
+    private void OnXenoRavageChargeHit(Entity<XenoRavageChargeComponent> xeno, ref ThrowDoHitEvent args)
+    {
+        // TODO RMC14 lag compensation
+        var targetId = args.Target;
+        if (_physicsQuery.TryGetComponent(xeno, out var physics) &&
+            _thrownItemQuery.TryGetComponent(xeno, out var thrown))
+        {
+            _thrownItem.LandComponent(xeno, thrown, physics, true);
+            _thrownItem.StopThrow(xeno, thrown);
+        }
+
+        if (_timing.IsFirstTimePredicted && xeno.Comp.Charge is { } charge)
+        {
+            xeno.Comp.Charge = null;
+            _xenoAnimations.PlayLungeAnimationEvent(xeno, charge);
+        }
+
+        if (!_xeno.CanAbilityAttackTarget(xeno, targetId, true))
+            return;
+
+        if (_net.IsServer)
+            _audio.PlayPvs(xeno.Comp.Sound, xeno);
+
+        var damage = _damageable.TryChangeDamage(targetId, xeno.Comp.Damage);
+        if (damage?.GetTotal() > FixedPoint2.Zero)
+        {
+            var filter = Filter.Pvs(targetId, entityManager: EntityManager).RemoveWhereAttachedEntity(o => o == xeno.Owner);
+            _colorFlash.RaiseEffect(Color.Red, new List<EntityUid> { targetId }, filter);
+        }
+
+        _rmcPulling.TryStopAllPullsFromAndOn(targetId);
+
+        var origin = _transform.GetMapCoordinates(xeno);
+        var target = _transform.GetMapCoordinates(targetId);
+        var diff = target.Position - origin.Position;
+        diff = diff.Normalized() * xeno.Comp.Range;
+
+        _stun.TryParalyze(targetId, xeno.Comp.StunTime, true);
+        _throwing.TryThrow(targetId, diff, 10);
+
+        if (_net.IsServer &&
+            HasComp<MarineComponent>(targetId))
+        {
+            SpawnAttachedTo(xeno.Comp.Effect, targetId.ToCoordinates());
+        }
+    }
+}
